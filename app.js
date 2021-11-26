0ad0829d22ccdb0c093cb1d92677d38d6a81778d
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2021-11-26 18:31:21 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var textareaInput = document.querySelector("#textarea-input");
+var outputDiv = document.querySelector("#output");
+var url = "	https://api.funtranslations.com/translate/yoda.json"
+
+function errorHandler(error) {
+    console.log(Error);
+    alert("Server is down! Try again after one hour.")
+}
+
+function clickEventHandler() {
+    var inputvalue = textareaInput.value;
+    fetch(GETTranslationUrl(inputvalue))
+    .then(response => response.json()
+    .then(json => 
+        outputDiv.innerHTML = json.contents.translated))
+    .catch(errorHandler);
+};
+
+function GETTranslationUrl(text){
+    updatedurl =  url + "?text="+text
+    return updatedurl;
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
+
